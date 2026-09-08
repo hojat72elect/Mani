7aa0557be4afe7af9b5dcf8a993e10b8d2d0eb81
hojat72elect <hojat72elect@gmail.com>
2026-09-08 15:33:14 -0400

    Completed the alpha version.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from MANI!")
-
+import sys
+from PySide6.QtWidgets import QApplication
+from ui.MainWindow import MainWindow
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    app.setApplicationName("PyImage Studio")
+    app.setOrganizationName("PyImage Studio")
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())

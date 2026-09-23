b047efa954db37e34e7e8fa76c749a7ae38fb46c
hojat72elect <hojat72elect@gmail.com>
2026-09-22 23:19:22 -0400

    docs

--- src/domain/PillowImageToQImageUseCase.py
+++ src/domain/PillowImageToQImageUseCase.py
@@ -2,6 +2,9 @@ from PIL import Image
 from PySide6.QtGui import QImage
 
 def PillowImageToQImageUseCase(image: Image.Image):
+    """
+    You give this function a pillow's "Image" and it will return a QT's "QImage" which you can show on a canvas.
+    """
     rgba = image.convert("RGBA")
     data = rgba.tobytes("raw", "RGBA")
     return QImage(

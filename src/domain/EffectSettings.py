337f85ceb4aa8983e682ef83bbe085cae2d8c140
hojat72elect <hojat72elect@gmail.com>
2026-09-22 22:57:24 -0400

    Added docs.

--- src/domain/EffectSettings.py
+++ src/domain/EffectSettings.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 
 @dataclass
 class EffectSettings:
+    """
+    The changes that the user can do to the picture at any given time, is defined in this form.
+    """
     brightness: float = 1.0
     contrast: float = 1.0
     saturation: float = 1.0
